8b63d7fac98d63b457a49a49fc952692f6e848f6
Aurelien Thierry <aurelien.thierry@inria.fr>
2014-03-07 17:21:51 +0100

    bouh

--- r2test2.py
+++ r2test2.py
@@ -1,8 +1,14 @@
 from r2 import r_core
 
 rc=r_core.RCore()
-rc.file_open("test3.bin", 0, 0)
+rc.file_open("test2.bin", 0, 0)
 rc.bin_load("", 0)
 # rc.assembler.set_arch("x86")
-rc.assembler.set_bits(32)
+# rc.assembler.set_bits(32)
+# rc.anal.set_bits(32)
+# sc=r_core.Rsyscall()
+# sc.setup("x86", 32, False, "linux")
+rc.config.set_i('asm.bits', 32)
+# rc.cmd0("e asm.bits = 32")
+# rc.bits = "32"
 print rc.cmd_str("ao")
